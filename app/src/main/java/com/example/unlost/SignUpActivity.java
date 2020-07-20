82fb4b498990f8ab340adc7bfbc2de27e73855d0
Shreyansh-Tripathi <shreyansht03@gmail.com>
2020-07-20 23:06:46 +0530

    Second Commit of the project.

--- app/src/main/java/com/example/unlost/SignUpActivity.java
+++ app/src/main/java/com/example/unlost/SignUpActivity.java
@@ -3,12 +3,28 @@ package com.example.unlost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText sgfirstName, sglastName, sgEmail, sgPassword;
+    Button signupbtn;
+    ImageView googleSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        sgEmail=findViewById(R.id.sgemail);
+        sgfirstName= findViewById(R.id.sgfirstname);
+        sglastName= findViewById(R.id.sglastname);
+        sgPassword= findViewById(R.id.sgpassword);
+
+        signupbtn= findViewById(R.id.signupbtn);
+
+        googleSignUp= findViewById(R.id.google_signup);
     }
 }
