4ea4b673bfc36414b98019e976e8114dec63b3ea
Shreyansh-Tripathi <shreyansht03@gmail.com>
2020-07-21 00:01:52 +0530

    Third Commit of the project.

--- app/src/main/java/com/example/unlost/LoginActivity.java
+++ app/src/main/java/com/example/unlost/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.example.unlost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView signuptxt;
     ImageView googlelogin;
     Button loginbtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
         googlelogin= findViewById(R.id.google_login);
         loginbtn= findViewById(R.id.logbtn);
 
+        mAuth = FirebaseAuth.getInstance();
+
         signuptxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +48,39 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uemail= logemail.getText().toString().trim();
+                String upassword= logpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(uemail) || TextUtils.isEmpty(upassword)){
+                    Toast.makeText(LoginActivity.this, "Empty Crediential(s)", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    loginUser(uemail, upassword);
+                }
+            }
+
+            private void loginUser(String email,String password) {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            startActivity(new Intent(LoginActivity.this, ChooseActivity.class));
+                            Toast.makeText(LoginActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                            this.finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this, "Error Logging You Up", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
 
         googlelogin.setOnClickListener(new View.OnClickListener() {
