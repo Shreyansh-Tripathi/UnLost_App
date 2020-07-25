e8cb62c82e51dbf7a38fb762bbebd00e2ad0e34a
Shreyansh-Tripathi <shreyansht03@gmail.com>
2020-07-25 18:57:20 +0530

    Time-06:56pm 25-07

--- app/src/main/java/com/example/unlost/activities/ChooseActivity.java
+++ app/src/main/java/com/example/unlost/activities/ChooseActivity.java
@@ -6,18 +6,33 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.unlost.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ChooseActivity extends AppCompatActivity {
 
     CardView noteActivity,lostActivity;
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        logout=findViewById(R.id.logout_btn);
         noteActivity=findViewById(R.id.noteActivity);
         lostActivity=findViewById(R.id.lostActivity);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ChooseActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
         noteActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
