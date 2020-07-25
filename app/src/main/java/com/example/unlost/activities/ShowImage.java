352331c81e5bd5578e7b67f40601d67065889735
Vaibhav5702 <V@ibh@v5702>
2020-07-25 19:07:32 +0530

    25/07/2020 7:07 pm

---
+++ app/src/main/java/com/example/unlost/activities/ShowImage.java
@@ -0,0 +1,35 @@
+package com.example.unlost.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.unlost.R;
+
+public class ShowImage extends AppCompatActivity {
+    ImageView imgShow;
+    ImageButton imgBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_image);
+        imgShow=findViewById(R.id.imgShow1);
+        imgBackBtn=findViewById(R.id.imgBackBtn);
+        final Intent intent=getIntent();
+        String imagePath=intent.getStringExtra("imagePath");
+        imgShow.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+        imgBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ShowImage.this,EditNoteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
